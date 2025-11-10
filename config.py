8f96a26b0e7998bc711c2ebd61a567ec59951312
ʟᴏɴᴇʟʏ ʙᴏʏ <117445045+Yohanrockstar20@users.noreply.github.com>
2025-11-10 11:19:37 +0530

    Update VERIFY_TUTORIAL URL in config.py

--- config.py
+++ config.py
@@ -56,7 +56,7 @@ VERIFY_MODE = bool(environ.get('VERIFY_MODE', True)) # Set True or False
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "gplinks.com") # shortlink domain without https://
 SHORTLINK_API = environ.get("SHORTLINK_API", "03a1f31a88c44180200c606b9903b37404440f16") # shortlink api
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://howtosolvedownload/22") # how to open link 
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/shortnerlinksolve/26") # how to open link 
 
 # Website Info:
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
